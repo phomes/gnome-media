251e4e1d2a553a755fbd3ba6a61a18fa66273d2b
Tim P. Gerla <timg@src.gnome.org>
1998-09-06 19:37:34 +0000

    Removed some unneeded files

--- tcd/socket.h
+++
@@ -1,9 +0,0 @@
-#ifndef TCD_SOCKET_H
-#define TCD_SOCKET_H
-
-typedef void (*PeriodicFunc)();
-
-int     opensocket( char *hostname, int port );
-int fgetsock( char* s, int size, int socket, PeriodicFunc func );
- 
-#endif
