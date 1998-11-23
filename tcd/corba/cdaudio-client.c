40621e8a5713b15ad7b8689e7ab34c0027a1e552
Tim P. Gerla <timg@src.gnome.org>
1998-11-23 03:37:11 +0000

    stuff for sopwith to look at, also a dir for upcoming corbaizing of gtcd

---
+++ tcd/corba/cdaudio-client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <orb/orbit.h>
+#include <libgnorba/gnorba.h>
+#include "cdaudio.h"
+
+GNOME_CDAudio cdaudio_client;
+
+int main(int argc, char *argv[])
+{
+    CORBA_Environment ev;
+    CORBA_ORB orb;
+    CORBA_long rv;
+    char buf[30];
+    GoadServer *servlist, *chosen = NULL;
+    int i;
+
+    CORBA_exception_init(&ev);
+    orb = gnome_CORBA_init("CDAudio Client", "0.0", &argc, argv, &ev);
+
+    
+    servlist = goad_server_list_get();
+    for(i = 0; servlist[i].repo_id; i++) 
+    {
+	g_print("ID %d: %s\n", i, servlist[i].server_id);
+	if(!strcmp(servlist[i].server_id, "cdaudio")) 
+	{
+	    chosen = &servlist[i];
+	    break;
+	}
+    }
+    cdaudio_client = goad_server_activate(chosen, GOAD_ACTIVATE_SHLIB);
+    sleep (5);
+
+    if(cdaudio_client == CORBA_OBJECT_NIL)
+    {
+	fprintf(stderr,"Cannot activate server\n");
+	exit(1);
+    }
+
+    g_print("%p\n", cdaudio_client);
+    GNOME_CDAudio_init_cd(cdaudio_client, "/dev/cdrom", &ev);
+    GNOME_CDAudio_play_cd(cdaudio_client, 1, 2, &ev);
+ 
+    CORBA_Object_release(cdaudio_client, &ev);
+    CORBA_Object_release((CORBA_Object)orb, &ev);
+
+    return 0;
+}
---
+++ tcd/corba/cdaudio-server.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <orb/orbit.h>
+#include <libgnorba/gnorba.h>
+#include "cdaudio.h"
+
+GNOME_CDAudio cdaudio_client = CORBA_OBJECT_NIL;
+
+static CORBA_Object do_cdaudio_init_cd(PortableServer_Servant servant,
+				       CORBA_char *path, 
+				       CORBA_Environment *ev);
+static CORBA_Object do_cdaudio_play_cd(PortableServer_Servant servant,
+				       CORBA_long track, 
+				       CORBA_Environment *ev);
+static CORBA_Object do_cdaudio_pause_cd(PortableServer_Servant servant,
+					CORBA_Environment *ev);
+static CORBA_Object do_cdaudio_stop_cd(PortableServer_Servant servant,
+				       CORBA_Environment *ev);
+static CORBA_Object do_cdaudio_eject_cd(PortableServer_Servant servant,
+					CORBA_Environment *ev);
+static CORBA_Object do_cdaudio_get_time(PortableServer_Servant servant,
+					GNOME_CDAudio_TimeFormat format,
+					CORBA_short *min,
+					CORBA_short *sec,
+					CORBA_Environment *ev);
+static CORBA_Object do_cdaudio_get_disc_info(PortableServer_Servant servant,
+					     CORBA_char **artist,
+					     CORBA_char **album,
+					     CORBA_char **title,
+					     CORBA_Environment *ev);
+
+PortableServer_ServantBase__epv base_epv = {
+  NULL,
+  NULL,
+  NULL
+};
+
+POA_GNOME_CDAudio__epv GNOME_CDAudio_epv = 
+{ 
+    NULL,
+    do_cdaudio_init_cd, 
+    do_cdaudio_play_cd,
+    do_cdaudio_pause_cd,
+    do_cdaudio_stop_cd,
+    do_cdaudio_eject_cd,
+    do_cdaudio_get_time,
+    do_cdaudio_get_disc_info
+};
+
+POA_GNOME_CDAudio__vepv poa_GNOME_CDAudio_vepv = { &base_epv, &GNOME_CDAudio_epv };
+POA_GNOME_CDAudio poa_GNOME_CDAudio_servant = { NULL, &poa_GNOME_CDAudio_vepv };
+
+int main(int argc, char *argv[])
+{
+    PortableServer_ObjectId objid = {0, sizeof("cdaudio_server"), "cdaudio_server"};
+    PortableServer_POA poa;
+
+    CORBA_Object name_service;
+    CORBA_Environment ev;
+    char *retval;
+    CORBA_ORB orb;
+
+    CORBA_exception_init(&ev);
+    orb = gnome_CORBA_init("GNOME_CDAudio Server", "0.0", &argc, argv, &ev);
+
+    POA_GNOME_CDAudio__init(&poa_GNOME_CDAudio_servant, &ev);
+    
+    poa = (PortableServer_POA)CORBA_ORB_resolve_initial_references(orb, "RootPOA", &ev);
+    PortableServer_POAManager_activate(PortableServer_POA__get_the_POAManager(poa, &ev), &ev);
+    
+    PortableServer_POA_activate_object_with_id(poa,
+                                               &objid, &poa_GNOME_CDAudio_servant, &ev);
+    
+    cdaudio_client = PortableServer_POA_servant_to_reference(poa,
+							  &poa_GNOME_CDAudio_servant,
+							  &ev);
+    g_print("getting name_service..\n");
+    name_service = gnome_name_service_get();
+    g_print("registering...\n");
+    goad_server_register(name_service, cdaudio_client, "cdaudio", "object", &ev);
+    g_print("registered\n");
+    retval = CORBA_ORB_object_to_string(orb, cdaudio_client, &ev);
+    g_print("%s\n", retval);
+    CORBA_free(retval);
+    CORBA_ORB_run(orb, &ev);
+    
+    return 0;
+}
+
+static CORBA_Object do_cdaudio_init_cd(PortableServer_Servant servant,
+				   CORBA_char *path, 
+				   CORBA_Environment *ev)
+{
+    g_print("do_cdaudio_init_cd stub\n");
+    return CORBA_Object_duplicate(cdaudio_client, ev);
+}
+
+
+static CORBA_Object do_cdaudio_play_cd(PortableServer_Servant servant,
+				   CORBA_long track, 
+				   CORBA_Environment *ev)
+{
+    g_print("do_cdaudio_play_cd stub\n");
+    return CORBA_Object_duplicate(cdaudio_client, ev);
+}
+
+static CORBA_Object do_cdaudio_pause_cd(PortableServer_Servant servant,
+					CORBA_Environment *ev)
+{
+    g_print("do_cdaudio_pause_cd stub\n");
+    return CORBA_Object_duplicate(cdaudio_client, ev);
+}
+
+static CORBA_Object do_cdaudio_stop_cd(PortableServer_Servant servant,
+				       CORBA_Environment *ev)
+{
+    g_print("do_cdaudio_stop_cd stub\n");
+    return CORBA_Object_duplicate(cdaudio_client, ev);
+}
+
+static CORBA_Object do_cdaudio_eject_cd(PortableServer_Servant servant,
+					CORBA_Environment *ev)
+{
+    g_print("do_cdaudio_eject_cd stub\n");
+    return CORBA_Object_duplicate(cdaudio_client, ev);
+}
+
+static CORBA_Object do_cdaudio_get_time(PortableServer_Servant servant,
+					GNOME_CDAudio_TimeFormat format,
+					CORBA_short *min,
+					CORBA_short *sec,
+					CORBA_Environment *ev)
+{
+    g_print("do_cdaudio_get_time stub\n");
+    return CORBA_Object_duplicate(cdaudio_client, ev);
+}
+
+static CORBA_Object do_cdaudio_get_disc_info(PortableServer_Servant servant,
+					     CORBA_char **artist,
+					     CORBA_char **album,
+					     CORBA_char **title,
+					     CORBA_Environment *ev)
+{
+    g_print("do_cdaudio_get_disc_info stub\n");
+    return CORBA_Object_duplicate(cdaudio_client, ev);
+}
