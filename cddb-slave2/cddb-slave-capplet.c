3c5e9b43a7b876e4fdefa6cac781b7fc0e7cb567
Kwok-Koon Cheung <baddog@src.gnome.org>
2002-06-11 12:00:16 +0000

    I18N fix for cddb-slave-capplet

--- cddb-slave2/cddb-slave-capplet.c
+++ cddb-slave2/cddb-slave-capplet.c
@@ -833,9 +833,9 @@ main (int argc,
 {
 	GtkWidget *dialog_win;
 	
-	bindtextdomain (PACKAGE, GNOMELOCALEDIR);
-	bind_textdomain_codeset (PACKAGE, "UTF-8");
-	textdomain (PACKAGE);
+	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
 
 	gnome_program_init (argv[0], VERSION, LIBGNOMEUI_MODULE, argc, argv, NULL);
 
