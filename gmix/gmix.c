757a3c3da356968da7fd387ebc2bee6407797138
Ted Gould <ted@src.gnome.org>
2003-06-24 05:55:02 +0000

    Changing everything so that I'm listed as the maintainer.  Also adding a FreeBSD patch that was given to Iain.

--- gmix/gmix.c
+++ gmix/gmix.c
@@ -591,6 +591,13 @@ open_device (int num)
 	/* Masks for the channel data - OSS blows compared to ALSA */
 	int recmask, recsrc, stereodee;
 
+#ifdef __FreeBSD__
+	/* FreeBSD device name detection */
+	FILE *sndstat;
+	char line[256];
+	char *devname, *p;
+#endif
+
 	/*
 	 * create new device configureation
 	 */
@@ -659,7 +666,26 @@ open_device (int num)
 	} 
 #else
 	new_device->card_name = g_strdup_printf ("OSS-%d-%d", num + 1, num + 1);
-	g_snprintf (new_device->info.name, 31, "Card %d", num+1);
+	/* get soundcard name from /dev/sndstat on FreeBSD */
+	sndstat = fopen("/dev/sndstat", "r");
+	if (!sndstat) {
+		g_snprintf (new_device->info.name, 31, "Card %d", num+1);
+	} else {
+		while (fgets (line, 255, sndstat)) {
+			if (!strncmp (line, "pcm", 3) && (atoi (&line[3]) == num))
+			{
+				devname = line;
+				while (*devname++ != '<')
+					;
+				p = devname;
+				while (*p != '>')
+					p++;
+				*p = '\0';
+				g_snprintf (new_device->info.name, 31, devname);
+			}
+		}
+		fclose(sndstat);
+	}
 #endif
 	/* 
 	 * several bitmasks describing the mixer
