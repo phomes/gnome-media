574f37bc78a0bfc92154eaaf57899ae6a5579366
Ted Gould <ted@src.gnome.org>
2003-10-05 09:51:01 +0000

    Fixing bug 123390.  Real work done by: Pav Lucistnik

--- cddb-slave2/cddb-slave-capplet.c
+++ cddb-slave2/cddb-slave-capplet.c
@@ -838,9 +838,6 @@ create_dialog (GtkWidget *window)
 	
 	gtk_container_add (GTK_CONTAINER (sw), pd->freedb_server);
 
-	g_signal_connect (G_OBJECT (pd->update), "clicked",
-			  G_CALLBACK (update_clicked), pd);
-
 	/* create the update server list button */
 	align = gtk_alignment_new (1.0, 0.5, 0.0, 0.0);
 
