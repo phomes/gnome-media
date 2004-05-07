349568bdb5fa22d64ad9b0be2814968760e7c937
Thomas Vander Stichele <thomasvs@src.gnome.org>
2004-05-07 10:21:38 +0000

    undo bug fix and indentation

--- gnome-cd/cddb.c
+++ gnome-cd/cddb.c
@@ -24,221 +24,221 @@
 
 static GHashTable *cddb_cache;
 static CDDBSlaveClient *slave = NULL;
-void freekey (gpointer key, gpointer value, gpointer userdata);
+void freekey(gpointer key, gpointer value,gpointer userdata);
 static int
-count_tracks (CDDBSlaveClientTrackInfo ** info)
+count_tracks (CDDBSlaveClientTrackInfo **info)
 {
-  int i;
+	int i;
 
-  for (i = 0; info[i] != NULL; i++) {
-    ;
-  }
+	for (i = 0; info[i] != NULL; i++) {
+		;
+	}
 
-  return i;
+	return i;
 }
 
 static void
-get_disc_info (GnomeCD * gcd, const char *discid)
+get_disc_info (GnomeCD *gcd,
+	       const char *discid)
 {
-  GnomeCDDiscInfo *info;
+	GnomeCDDiscInfo *info;
+	
+	info = g_hash_table_lookup (cddb_cache, discid);
+	if (info == NULL) {
+		g_warning ("No cache for %s", discid);
+		return;
+	} else {
+		gcd->disc_info = info;
+	}
 
-  info = g_hash_table_lookup (cddb_cache, discid);
-  if (info == NULL) {
-    g_warning ("No cache for %s", discid);
-    return;
-  } else {
-    gcd->disc_info = info;
-  }
+	info->title = cddb_slave_client_get_disc_title (slave, discid);
+	info->artist = cddb_slave_client_get_artist (slave, discid);
+	info->track_info = cddb_slave_client_get_tracks (slave, discid);
 
-  info->title = cddb_slave_client_get_disc_title (slave, discid);
-  info->artist = cddb_slave_client_get_artist (slave, discid);
-  info->track_info = cddb_slave_client_get_tracks (slave, discid);
+	if (count_tracks (info->track_info) != info->ntracks) {
+		/* Duff info */
+		cddb_slave_client_free_track_info (info->track_info);
+		info->track_info = NULL;
+		gcd->disc_info = NULL;
+	}
 
-  if (count_tracks (info->track_info) != info->ntracks) {
-    /* Duff info */
-    cddb_slave_client_free_track_info (info->track_info);
-    info->track_info = NULL;
-    gcd->disc_info = NULL;
-  }
-
-  gnome_cd_build_track_list_menu (gcd);
+	gnome_cd_build_track_list_menu (gcd);
 }
 
 static void
-cddb_listener_event_cb (BonoboListener * listener,
-    const char *name,
-    const BonoboArg * arg, CORBA_Environment * ev, GnomeCD * gcd)
+cddb_listener_event_cb (BonoboListener *listener,
+			const char *name,
+			const BonoboArg *arg,
+			CORBA_Environment *ev,
+			GnomeCD *gcd)
 {
-  GNOME_Media_CDDBSlave2_QueryResult *qr;
+	GNOME_Media_CDDBSlave2_QueryResult *qr;
 
-  qr = arg->_value;
+	qr = arg->_value;
 
-  switch (qr->result) {
-    case GNOME_Media_CDDBSlave2_OK:
-      get_disc_info (gcd, qr->discid);
-      break;
+	switch (qr->result) {
+	case GNOME_Media_CDDBSlave2_OK:
+		get_disc_info (gcd, qr->discid);
+		break;
 
-    case GNOME_Media_CDDBSlave2_REQUEST_PENDING:
-      /* Do nothing really */
-      gcd->disc_info = NULL;
-      break;
+	case GNOME_Media_CDDBSlave2_REQUEST_PENDING:
+		/* Do nothing really */
+		gcd->disc_info = NULL;
+		break;
 
-    case GNOME_Media_CDDBSlave2_ERROR_CONTACTING_SERVER:
-      g_warning ("Could not contact CDDB server");
-      gcd->disc_info = NULL;
-      break;
+	case GNOME_Media_CDDBSlave2_ERROR_CONTACTING_SERVER:
+		g_warning ("Could not contact CDDB server");
+		gcd->disc_info = NULL;
+		break;
 
-    case GNOME_Media_CDDBSlave2_ERROR_RETRIEVING_DATA:
-      g_warning ("Error downloading data");
-      gcd->disc_info = NULL;
-      break;
+	case GNOME_Media_CDDBSlave2_ERROR_RETRIEVING_DATA:
+		g_warning ("Error downloading data");
+		gcd->disc_info = NULL;
+		break;
 
-    case GNOME_Media_CDDBSlave2_MALFORMED_DATA:
-      g_warning ("Malformed data");
-      gcd->disc_info = NULL;
-      break;
+	case GNOME_Media_CDDBSlave2_MALFORMED_DATA:
+		g_warning ("Malformed data");
+		gcd->disc_info = NULL;
+		break;
 
-    case GNOME_Media_CDDBSlave2_IO_ERROR:
-      g_warning ("Generic IO error");
-      gcd->disc_info = NULL;
-      break;
+	case GNOME_Media_CDDBSlave2_IO_ERROR:
+		g_warning ("Generic IO error");
+		gcd->disc_info = NULL;
+		break;
 
-    default:
-      break;
-  }
+	default:
+		break;
+	}
 }
 
 void
-cddb_free_disc_info (GnomeCDDiscInfo * info)
+cddb_free_disc_info (GnomeCDDiscInfo *info)
 {
-  g_free (info->discid);
-  g_free (info->title);
-  g_free (info->artist);
+	g_free (info->discid);
+	g_free (info->title);
+	g_free (info->artist);
 
-  if (info->track_info)
-    cddb_slave_client_free_track_info (info->track_info);
-  g_free (info);
+	if (info->track_info)
+		cddb_slave_client_free_track_info (info->track_info);
+	g_free (info);
 }
 
 static GnomeCDDiscInfo *
-cddb_make_disc_info (GnomeCDRomCDDBData * data)
+cddb_make_disc_info (GnomeCDRomCDDBData *data)
 {
-  GnomeCDDiscInfo *discinfo;
+	GnomeCDDiscInfo *discinfo;
 
-  discinfo = g_new0 (GnomeCDDiscInfo, 1);
-  discinfo->discid = g_strdup_printf ("%08lx", (gulong) data->discid);
-  discinfo->title = NULL;
-  discinfo->artist = NULL;
-  discinfo->ntracks = data->ntrks;
-  discinfo->track_info = NULL;
+	discinfo = g_new0 (GnomeCDDiscInfo, 1);
+	discinfo->discid = g_strdup_printf ("%08lx", (gulong) data->discid);
+	discinfo->title = NULL;
+	discinfo->artist = NULL;
+	discinfo->ntracks = data->ntrks;
+	discinfo->track_info = NULL;
 
-  return discinfo;
+	return discinfo;
 }
 
 void
 cddb_close_client (void)
 {
-  if (slave != NULL)
-    g_object_unref (G_OBJECT (slave));
+	if (slave != NULL)
+		g_object_unref (G_OBJECT (slave));
 }
-
 // when the CD is ejected we need to destroy the cddb_cache hashTable
-void
-destroy_cache_hashTable ()
+void destroy_cache_hashTable()
 {
-  g_hash_table_foreach (cddb_cache, freekey, NULL);	//first free the key
-  g_hash_table_destroy (cddb_cache);	//destroy the table
-  cddb_cache = NULL;
+	g_hash_table_foreach(cddb_cache,freekey,NULL); //first free the key
+	g_hash_table_destroy(cddb_cache); //destroy the table
+	cddb_cache = NULL;
 }
 
 //this will only free the keys in hashtable, the values are already freed.
-void
-freekey (gpointer key, gpointer value, gpointer userdata)
+void freekey(gpointer key, gpointer value,gpointer userdata)
 {
-  if (key != NULL)
-    g_free (key);
-  key = NULL;
+		if(key != NULL)
+			g_free(key);
+		key = NULL;
 }
 
 void
-cddb_get_query (GnomeCD * gcd)
+cddb_get_query (GnomeCD *gcd)
 {
-  GnomeCDRomCDDBData *data;
-  GnomeCDDiscInfo *info;
-  BonoboListener *listener;
-  char *discid;
-  char *offsets = NULL;
-  int i;
+	GnomeCDRomCDDBData *data;
+	GnomeCDDiscInfo *info;
+	BonoboListener *listener;
+	char *discid;
+	char *offsets = NULL;
+	int i;
 
-  if (cddb_cache == NULL) {
-    cddb_cache = g_hash_table_new (g_str_hash, g_str_equal);
-  }
+	if (cddb_cache == NULL) {
+		cddb_cache = g_hash_table_new (g_str_hash, g_str_equal); 
+	}
 
-  if (gnome_cdrom_get_cddb_data (gcd->cdrom, &data, NULL) == FALSE) {
-    g_print ("gnome_cdrom_get_cddb_data returned FALSE");
-    return;
-  }
+	if (gnome_cdrom_get_cddb_data (gcd->cdrom, &data, NULL) == FALSE) {
+		g_print ("gnome_cdrom_get_cddb_data returned FALSE");
+		return;
+	}
 
-  discid = g_strdup_printf ("%08lx", (gulong) data->discid);
-  for (i = 0; i < data->ntrks; i++) {
-    char *tmp, *tmp2;
+	discid = g_strdup_printf ("%08lx", (gulong) data->discid);
+	for (i = 0; i < data->ntrks; i++) {
+		char *tmp, *tmp2;
 
-    tmp = g_strdup_printf ("%u ", data->offsets[i]);
-    if (offsets == NULL) {
-      offsets = tmp;
-    } else {
-      tmp2 = g_strconcat (offsets, tmp, NULL);
-      g_free (tmp);
-      offsets = g_strdup (tmp2);
-      g_free (tmp2);
-    }
-  }
+		tmp = g_strdup_printf ("%u ", data->offsets[i]);
+		if (offsets == NULL) {
+			offsets = tmp;
+		} else {
+			tmp2 = g_strconcat (offsets, tmp, NULL);
+			g_free (tmp);
+			offsets = g_strdup (tmp2);
+			g_free (tmp2);
+		}
+	}
 
-  info = g_hash_table_lookup (cddb_cache, discid);
-  g_free (discid);
+	info = g_hash_table_lookup (cddb_cache, discid);
+	g_free (discid);
 
-  if (info != NULL) {
-    gcd->disc_info = info;
+	if (info != NULL) {
+		gcd->disc_info = info;
 
-    gnome_cd_build_track_list_menu (gcd);
-    return;
-  } else {
-    info = cddb_make_disc_info (data);
-    // g_strdup is added so that the key will persist
-    g_hash_table_insert (cddb_cache, g_strdup (info->discid), info);
-    gcd->disc_info = info;
-    gnome_cd_build_track_list_menu (gcd);
-  }
+		gnome_cd_build_track_list_menu (gcd);
+		return;
+	} else {
+		info = cddb_make_disc_info (data);
+		// g_strdup is added so that the key will persist
+		g_hash_table_insert (cddb_cache, g_strdup(info->discid), info);
+		gcd->disc_info = info;
+		gnome_cd_build_track_list_menu (gcd);
+	}
 
-  /* Remove the last space */
-  offsets[strlen (offsets) - 1] = 0;
+	/* Remove the last space */
+	offsets[strlen (offsets) - 1] = 0;
 
-  if (slave == NULL) {
-    slave = cddb_slave_client_new ();
-    listener = bonobo_listener_new (NULL, NULL);
-    g_signal_connect (G_OBJECT (listener), "event-notify",
-	G_CALLBACK (cddb_listener_event_cb), gcd);
+	if (slave == NULL) {
+		slave = cddb_slave_client_new ();
+		listener = bonobo_listener_new (NULL, NULL);
+		g_signal_connect (G_OBJECT (listener), "event-notify",
+				  G_CALLBACK (cddb_listener_event_cb), gcd);
 
-    cddb_slave_client_add_listener (slave, listener);
-  }
+		cddb_slave_client_add_listener (slave, listener);
+	}
 
-  cddb_slave_client_query (slave, discid, data->ntrks, offsets,
-      data->nsecs, "GnomeCD", VERSION);
-
-  gnome_cdrom_free_cddb_data (data);
+	cddb_slave_client_query (slave, discid, data->ntrks, offsets, 
+				 data->nsecs, "GnomeCD", VERSION);
+	
+	gnome_cdrom_free_cddb_data (data);
 }
 
 int
 cddb_sum (int n)
 {
-  char buf[12], *p = NULL;
-  int ret = 0;
+	char buf[12], *p;
+	int ret = 0;
+	
+	/* This is what I get for copying TCD code */
+	sprintf (buf, "%u", n);
+	for (p = buf; *p != '\0'; p++) {
+		ret += (*p - '0');
+	}
 
-  /* This is what I get for copying TCD code */
-  sprintf (buf, "%u", n);
-  for (p = buf; *p != '\0'; p++) {
-    ret += (*p - '0');
-  }
-
-  return ret;
+	return ret;
 }
