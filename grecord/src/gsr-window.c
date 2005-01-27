c6a45d6af4b1ec9e653e519849d4f63cda7f4144
Ronald Bultje <rbultje@src.gnome.org>
2005-01-27 23:12:50 +0000

    Reviewed by:  Ronald S. Bultje  <rbultje@ronald.bitfreak.net> * configure.in: * grecord/src/gsr-window.c: (do_save_file): Use GNOME-VFS to save files instead of /bin/cp (#162185).

--- grecord/src/gsr-window.c
+++ grecord/src/gsr-window.c
@@ -29,7 +29,7 @@
 #include <unistd.h>
 
 #include <gnome.h>
-#include <libgnomevfs/gnome-vfs-utils.h>
+#include <libgnomevfs/gnome-vfs.h>
 #include <gconf/gconf-client.h>
 #include <gst/gst.h>
 #include <gst/gconf/gconf.h>
@@ -476,30 +476,47 @@ do_save_file (GSRWindow *window,
 {
 	GSRWindowPrivate *priv;
         GMAudioProfile *profile;
-	char *tmp, *command;
-	GError *error;
-	int status;
-	
-	status = 0;
+	char *tmp, *src;
+	GnomeVFSURI *src_uri, *dst_uri;
 
 	priv = window->priv;
-	
+
         profile = gm_audio_profile_choose_get_active (priv->profile);
 
-	tmp = g_strdup_printf ("%s.%s", name,
+	tmp = g_strdup_printf ("file://%s.%s", name,
 			       gm_audio_profile_get_extension (profile));
-	
-	command = g_strdup_printf ("/bin/cp %s %s", priv->record_filename, tmp);
 
-	if (!g_spawn_command_line_sync (command,  NULL, NULL, &status, &error)) {
-		gchar *error_message;
-
-		error_message = g_strdup_printf (_("Could not save the file \"%s\""), error->message);
-		show_error_dialog (GTK_WINDOW (window), error_message);
-		g_free (error_message);
-	}
-	
-	if (status) {
+	src = g_strdup_printf ("file://%s", priv->record_filename);
+	src_uri = gnome_vfs_uri_new (src);
+	dst_uri = gnome_vfs_uri_new (tmp);
+	g_free (src);
+	if (src_uri && dst_uri) {
+		GnomeVFSResult result;
+		result = gnome_vfs_xfer_uri (src_uri, dst_uri,
+			GNOME_VFS_XFER_DEFAULT,
+			GNOME_VFS_XFER_ERROR_MODE_ABORT,
+			GNOME_VFS_XFER_OVERWRITE_MODE_REPLACE,
+			NULL, NULL);
+		if (result == GNOME_VFS_OK) {
+			char *title, *short_name;
+			priv->filename = g_strdup_printf ("%s.%s",
+				name, gm_audio_profile_get_extension (profile));
+			short_name = g_path_get_basename (priv->filename);
+			title = g_strdup_printf ("%s - Sound Recorder",
+				short_name);
+			gtk_window_set_title (GTK_WINDOW (window), title);
+			priv->dirty = FALSE;
+			g_free (title);
+		} else {
+			gchar *error_message;
+ 
+			error_message = g_strdup_printf (_("Could not save the file \" %s\""), gnome_vfs_result_to_string (result));
+			show_error_dialog (GTK_WINDOW (window), error_message);
+			g_free (error_message);
+		}
+		gnome_vfs_uri_unref (src_uri);
+		gnome_vfs_uri_unref (dst_uri);
+	} else {
 		gchar *error_message;
 
 		error_message = g_strdup_printf (_("Could not save the file \"%s\""), tmp);
@@ -507,8 +524,7 @@ do_save_file (GSRWindow *window,
 		g_free (error_message);
 	}
 
-	g_free (tmp);		
-	g_free (command);		
+	g_free (tmp);
 }
 
 static void
