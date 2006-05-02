da65cff4b64c070d2bab399dd2c530cec0dfa4a0
Stanislav Brabec <utx@src.gnome.org>
2006-05-02 15:56:02 +0000

    Fixed I18N support (#338251).

--- gstreamer-properties/gstreamer-properties.c
+++ gstreamer-properties/gstreamer-properties.c
@@ -398,7 +398,7 @@ main (int argc, char **argv)
   GOptionContext *ctx;
   GOptionGroup *group;
 
-  /* bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR); */
+  bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
 
