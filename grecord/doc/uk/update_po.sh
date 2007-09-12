cc711d446733a83a54a599992a588757f69d1e8b
Maxim Dziumanenko <dziumanenko@gmail.com>
2007-09-12 21:51:29 +0000

    Update Ukrainian translation.
    2007-09-12 Maxim Dziumanenko <dziumanenko@gmail.com>
    
    	* Update Ukrainian translation.
    
    svn path=/trunk/; revision=3699

--- grecord/doc/uk/update_po.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xml2po -e -u uk.po \
-    ../../../grecord/doc/C/gnome-sound-recorder.xml \
-    ../../../gstreamer-properties/help/C/gstreamer-properties.xml \
-    ../../../gnome-cd/doc/C/gnome-cd.xml \
-    ../../../gst-mixer/doc/C/gnome-volume-control.xml
