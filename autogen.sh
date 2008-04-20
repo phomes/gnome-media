bacdabcae3692538146a287870c9c7352bf06f69
Marc-André Lureau <marcandre.lureau@gmail.com>
2008-04-20 00:12:24 +0000

    depends on intltool 0.39.99 no more necessary to ship
    2008-04-20  Marc-André Lureau  <marcandre.lureau@gmail.com>
    
    	* autogen.sh, configure.in: depends on intltool 0.39.99
    	* Makefile.am (INTLTOOL_BUILT): no more necessary to ship
    
    svn path=/trunk/; revision=3844

--- autogen.sh
+++ autogen.sh
@@ -19,9 +19,7 @@ which gnome-autogen.sh || {
     exit 1
 }
 
-# intltoolize 0.33 has a bug; it contains commands to use patches that
-# were removed
-REQUIRED_INTLTOOL_VERSION=0.35
+REQUIRED_INTLTOOL_VERSION=0.39.99
 
 # fix doc build before turning on common doc build
 # USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
