5cd43e82e94c0c5717e9026eb1aa9e15c6dfedff
Bastien Nocera <hadess@hadess.net>
2009-08-13 15:08:14 +0100

    Never push the volume to event streams
    Those aren't supposed to be changed that way, but through the
    event role.

--- gnome-volume-control/src/gvc-mixer-stream.c
+++ gnome-volume-control/src/gvc-mixer-stream.c
@@ -650,7 +650,15 @@ gvc_mixer_stream_push_volume (GvcMixerStream *stream)
 {
         pa_operation *op;
         gboolean ret;
+
         g_return_val_if_fail (GVC_IS_MIXER_STREAM (stream), FALSE);
+
+        if (stream->priv->is_event_stream != FALSE)
+                return TRUE;
+
+        g_debug ("Pushing new volume to stream '%s' (%s)",
+                 stream->priv->description, stream->priv->name);
+
         ret = GVC_MIXER_STREAM_GET_CLASS (stream)->push_volume (stream, (gpointer *) &op);
         if (ret) {
                 if (stream->priv->change_volume_op != NULL)
