4b0e05347315df74de479bb839bde6936a08309d
Bastien Nocera <hadess@hadess.net>
2009-09-10 11:22:10 +0100

    Bug 592326 - g-v-c defaults to 0% for event sounds if no event sound rule is set
    When we don't have any saved volume for the event stream, still create
    the stream after processing the restore.

--- gnome-volume-control/src/gvc-mixer-control.c
+++ gnome-volume-control/src/gvc-mixer-control.c
@@ -1328,6 +1328,17 @@ _pa_ext_stream_restore_read_cb (pa_context                       *context,
 
         if (eol > 0) {
                 dec_outstanding (control);
+                /* If we don't have an event stream to restore, then
+                 * set one up with a default 100% volume */
+                if (!control->priv->event_sink_input_is_set) {
+                        pa_ext_stream_restore_info info;
+
+                        memset (&info, 0, sizeof(info));
+                        info.name = "sink-input-by-media-role:event";
+                        info.volume.channels = 1;
+                        info.volume.values[0] = PA_VOLUME_NORM;
+                        update_event_role_stream (control, &info);
+                }
                 return;
         }
 
