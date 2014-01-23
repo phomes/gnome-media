945d284326f841cfb80be7f7e856002f3c7bd6eb
Daniel Mustieles <daniel.mustieles@gmail.com>
2014-01-23 12:47:15 +0100

    Updated FSF's address

--- grecord/src/gsr-window.c
+++ grecord/src/gsr-window.c
@@ -17,9 +17,7 @@
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  *
  * 4th Februrary 2005: Christian Schaller: changed license to LGPL with
  * permission of Iain Holmes, Ronald Bultje, Leontine Binchy (SUN), Johan Dalhin
